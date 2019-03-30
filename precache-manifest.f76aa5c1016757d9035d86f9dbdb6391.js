6a5c047f5e9fa582c406d624128e6bbe9fe252c9
borulchenko <ruslan.borulchenko@gmail.com>
2019-03-30 14:03:29 +0300

    Updates

---
+++ precache-manifest.f76aa5c1016757d9035d86f9dbdb6391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "238cb4c9ba5a19f5f0c2",
+    "url": "/contactmanager/static/css/main.e2a91002.chunk.css"
+  },
+  {
+    "revision": "238cb4c9ba5a19f5f0c2",
+    "url": "/contactmanager/static/js/main.c3341946.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "141ba77735f60a1969c3",
+    "url": "/contactmanager/static/css/2.815f6d92.chunk.css"
+  },
+  {
+    "revision": "141ba77735f60a1969c3",
+    "url": "/contactmanager/static/js/2.20d82ee2.chunk.js"
+  },
+  {
+    "revision": "cf7eb9d13eade8c6950efa6033555c26",
+    "url": "/contactmanager/index.html"
+  }
+];
