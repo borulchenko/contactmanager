815225403567971dcde8160969889da352987bdf
borulchenko <ruslan.borulchenko@gmail.com>
2019-03-31 22:35:49 +0300

    Post and Delete request dispatch

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -3,7 +3,6 @@ import TextInputGroup from "../layout/TextInputGroup";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { addContact } from "../../actions/contactActions";
-import uuid from "uuid";
 
 class AddContact extends Component {
   state = {
@@ -27,7 +26,6 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
       name,
       phone
     };
